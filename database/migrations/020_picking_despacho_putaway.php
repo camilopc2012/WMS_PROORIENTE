6b93842fa1cd00d6e87e6595b08d99b67db56072
camilopc2012 <117849532+camilopc2012@users.noreply.github.com>
2026-03-23 15:53:42 -0500

    feat: WMS v2 - 020_picking_despacho_putaway.php

---
+++ database/migrations/020_picking_despacho_putaway.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+/** Migración 020 - WMS v2 */
+return [
+    'up'=>function(){ echo "Migracion 020\n"; },
+    'down'=>function(){ echo "Rollback 020\n"; },
+];
