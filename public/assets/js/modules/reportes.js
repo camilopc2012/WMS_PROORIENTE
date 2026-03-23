2fcee7c7d6ed9a1e3d96d1621adaf12a5ca520b0
camilopc2012 <117849532+camilopc2012@users.noreply.github.com>
2026-03-23 16:05:48 -0500

    feat: WMS v2 - reportes.js

---
+++ public/assets/js/modules/reportes.js
@@ -0,0 +1,15 @@
+// reportes.js - WMS v2 - Centro de Reportes
+window.Reportes = {
+    init() {
+        console.log('Reportes inicializado');
+    },
+    abrirKardex() {
+        console.log('Kardex abierto');
+    },
+    abrirStock() {
+        console.log('Stock abierto');
+    },
+    exportar(id) {
+        console.log('Exportar reporte:', id);
+    }
+};
